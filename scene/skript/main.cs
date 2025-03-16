79dc2c3cd3e38e1dc09016bcaabd275681474cb5
chert <komisatoa0@gmail.com>
2025-03-16 20:24:13 +0300

    who come make to the save mecanic

--- scene/skript/main.cs
+++ scene/skript/main.cs
@@ -4,15 +4,4 @@ using Godot;
 public partial class main : Node2D
 {
 
-    public override void _PhysicsProcess(double delta)
-    {
-        if (ui_pc_player.time == true)
-        {
-            Engine.TimeScale = 0.001;
-        }
-        else
-        {
-            Engine.TimeScale = 1;
-        }
-    }
 }
--- scene/skript/ui_pc_player.cs
+++ scene/skript/ui_pc_player.cs
@@ -1,15 +1,22 @@
 using Godot;
 using System;
+using System.Text.RegularExpressions;
 using System.Xml.XPath;
 
 public partial class ui_pc_player : Control
 {
-	private int chest_nomber=0;
-	[Export] public PackedScene chest {get;set;}
-	public static bool time = false;
+	public static int on_open_chest = 0;
+
+	private bool true_chest = false;
+
+	[Export] public PackedScene chest;
+
 	[Export]Label text{get;set;}
+
 	[Export]private ProgressBar value{get;set;}
-	[Export]Player player ;
+
+	[Export]Player player;
+
     public void _on_xp()
 	{
 	  GD.Print("player xp =" +Player.xp);
@@ -18,21 +25,59 @@ public partial class ui_pc_player : Control
 
 	public void _on_vale_text(string money)
 	{
-		text.Text = money;
+		text.Text = "  "+money;
 	}
 	private void _open_chest()
 	{
-		if (chest_nomber == 1)
-		{
-			return;
-		}
-		chest_nomber= 1;
-
-		var cheste = (shest)chest.Instantiate() as shest;
-	  AddChild(cheste);
-	  cheste.GlobalPosition = player.GlobalPosition;
-
-	   time = true;
-	   
+      True_open_chest();
+    
+	if (chest == null)
+	{
+		GD.PushError("null chest");
+		return;
 	}
+	  
+
+      switch (on_open_chest)
+	  {
+		case  1 :
+		      GD.Print("one one zero one ");
+		      if (true_chest== true)
+			  {
+				OpenChest();
+			  }
+		break;
+
+		case 0 :
+		     if (true_chest== false)
+			 {
+				
+				GD.PushError(on_open_chest);
+
+				on_open_chest++;
+				_open_chest();
+			 }
+		break;
+
+		default:
+		       on_open_chest = 1;
+		break;
+	  }
+
+	}
+
+	private void True_open_chest()
+	{
+	  on_open_chest = Math.Clamp(on_open_chest , 0 , 1 );
+	  //on_open_chest = 0;
+      true_chest = Convert.ToBoolean(on_open_chest);
+	}
+
+	private void OpenChest()
+	{
+    var scene = (shest)chest.Instantiate();			
+	GetParent().AddChild(scene);
+	scene.GlobalPosition = player.GlobalPosition;
+	}
+	
 }
